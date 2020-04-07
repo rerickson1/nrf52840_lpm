68e4a0bac4de09e746facf781825a2dbb3128728
Ryan Erickson <ryan.erickson@lairdconnect.com>
2020-04-07 12:04:19 -0500

    Disable RX IRQ
    Disable RX IRQ if applicable.

--- app/src/power.c
+++ app/src/power.c
@@ -1,15 +1,30 @@
 #include <zephyr.h>
 #include <power.h>
+#include <device.h>
+#include <uart.h>
 
 static void shutdown_console_uart(void)
 {
 	struct device *uart_dev;
 	uart_dev = device_get_binding(CONFIG_UART_CONSOLE_ON_DEV_NAME);
+
+#ifdef CONFIG_UART_INTERRUPT_DRIVEN
+	uart_irq_rx_disable(uart_dev);
+#endif
+
 	int rc = device_set_power_state(uart_dev, DEVICE_PM_OFF_STATE, NULL,
 					NULL);
 	if (rc) {
 		printk("Error disabling console UART peripheral (%d)\n", rc);
 	}
+
+	/* power cycle UART perpheral.
+	* WARNING: this clears all registers of the peripheral
+	*/
+	/* *(volatile uint32_t *)0x40002FFC = 0;
+	 *(volatile uint32_t *)0x40002FFC;
+	 *(volatile uint32_t *)0x40002FFC = 1;
+	 */
 }
 
 static void startup_console_uart(void)
